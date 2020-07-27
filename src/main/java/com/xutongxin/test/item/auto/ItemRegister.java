adde5aa770ca5eb7570ba50f96e12c4c578a5def
Yecgaa1 <38446391+Yecgaa1@users.noreply.github.com>
2020-07-28 00:05:50 +0800

    123

---
+++ src/main/java/com/xutongxin/test/item/auto/ItemRegister.java
@@ -0,0 +1,15 @@
+package com.xutongxin.test.item.auto;
+
+import com.github.dawnflyc.processtree.ITreeHandler;
+import com.github.dawnflyc.processtree.Result;
+import com.github.dawnflyc.processtree.TreeScan;
+import com.xutongxin.test.event.GameRegister;
+import net.minecraft.item.Item;
+
+@TreeScan(packageName = "com.xutongxin.test.item.auto",method = Item.class)
+public class ItemRegister implements ITreeHandler<Item> {
+    @Override
+    public void handle(Result<Item> result) {
+        GameRegister.RegistryItems(result.build());
+    }
+}
---
+++ src/main/java/com/xutongxin/test/item/auto/TestItem.java
@@ -0,0 +1,11 @@
+package com.xutongxin.test.item.auto;
+
+import net.minecraft.item.Item;
+
+public class TestItem extends Item {
+
+    public TestItem() {
+        super(new Properties());
+        this.setRegistryName("helloworld");
+    }
+}
