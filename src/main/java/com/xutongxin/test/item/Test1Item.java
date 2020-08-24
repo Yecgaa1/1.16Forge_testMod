aed166231b0702009de3ec1a496f249f878aa0dd
Yecgaa1 <38446391+Yecgaa1@users.noreply.github.com>
2020-08-24 19:24:52 +0800

    123

--- src/main/java/com/xutongxin/test/item/Test1Item.java
+++ src/main/java/com/xutongxin/test/item/Test1Item.java
@@ -1,12 +1,14 @@
 package com.xutongxin.test.item;
 
+import com.xutongxin.test.xtxgroup;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 
 public class Test1Item extends Item {
 
-    public Test1Item(String registryName,float sweet) {
-        super(new Properties().food(new Food.Builder().saturation(sweet).hunger((int) sweet).build()));
+    public Test1Item(String registryName, float sweet) {
+        super(new Properties().food(new Food.Builder().saturation(sweet).hunger((int) sweet).build()).group(xtxgroup.Modgroup.itemGroup));
         this.setRegistryName(registryName);
+        
     }
 }
