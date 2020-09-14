a6750fc8dd68e52d395619f23d1b8573975a25e5
Yecgaa1 <38446391+Yecgaa1@users.noreply.github.com>
2020-09-14 22:29:43 +0800

    123

---
+++ src/main/java/com/xutongxin/test/event/Join.java
@@ -0,0 +1,22 @@
+package com.xutongxin.test.event;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class Join {
+    @SubscribeEvent
+    public static void onPlayerJoin(EntityJoinWorldEvent event)
+    {
+        Entity entity=event.getEntity();
+        if(entity instanceof PlayerEntity)
+        {
+            String message="Hello world";
+            StringTextComponent text=new StringTextComponent(message);
+            entity.sendMessage(text);
+
+        }
+    }
+}
